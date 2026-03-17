fc20799400b4f2e61a9099ae02e424c11a0cb406
Fiodar mac <lazukfedor07@gmil.com>
2026-03-17 21:50:01 +0100

    Initial project

---
+++ gitTrain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 3, 7, 2, 9, 5 };
+        
+        Console.WriteLine("Numbers:");
+        
+        foreach (int n in numbers)
+            {
+            Console.Write(n + " ");
+            }
+        Console.WriteLine();
+        Console.WriteLine("Sum: " + Sum(numbers));
+    }
+
+    static int Sum(int[] arr)
+    {
+        int sum = 0;
+
+        foreach (int n in arr)
+        {
+            sum += n;
+        }
+        
+        return sum;
+    }
+}
