183ccff8a043a86af549c1679d89eb8147ddd22f
Fiodar mac <lazukfedor07@gmil.com>
2026-03-17 22:08:10 +0100

    Add empty array check

--- gitTrain/NumCalc.cs
+++ gitTrain/NumCalc.cs
@@ -4,6 +4,11 @@ public class NumCalc
 { 
     public static int Sum(int[] arr)
     {
+        if (arr.Length == 0)
+        {
+            return 0;
+        }
+        
         int sum = 0;
 
         foreach (int n in arr)
